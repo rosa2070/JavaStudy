f0c532ef4e1f5d3e3fa6b5099c211135fdefa54c
rosa2070 <rosa2070@naver.com>
2023-12-13 22:08:10 +0900

    first

---
+++ src/CarTest.java
@@ -0,0 +1,22 @@
+public class CarTest {
+    public static void main(String[] args) {
+        // 자동차의 정보를 저장 하려고 한다 변수를 만들어 보시오
+        String model = "BMW528i"; // 모델 : 문자열 "(쌍따옴표)
+        System.out.println("model = " + model);
+        long distance = 10000000000L; // 주행거리
+        System.out.println("distance = " + distance + "km");
+        int price = 9000000;
+        System.out.println("price = " + price);
+        String company = "BMW";
+        System.out.println("company = " + company);
+        char type='A';
+        System.out.println("type = " + type + "Type");
+        boolean auto = true; // 변속
+        System.out.println("auto = " + auto);
+        int year = 2000; // 연식
+        System.out.println("year = " + year);
+        float gasmi = 12.5f; //연비
+        System.out.println("gasmi = " + gasmi + "l");
+
+    }
+}
---
+++ src/FindAPI.java
@@ -0,0 +1,14 @@
+public class FindAPI {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        // AAA b; 컴파일러가 AAA를 가지고 있지 않기에 에러 뜸
+        a=10;
+        System.out.println("a = " + a);
+        // "APPLE" 문자열 저장하고 출력
+        String s; //java.lang.String인데 java.lang이 생략되어 있음
+        s = "APPLE";
+        System.out.println("s = " + s);
+        System.out.println(s.length()); //string class가 가진 length라는 동작
+
+    }
+}
